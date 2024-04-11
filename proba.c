ea32fad7dd3e26be71d6a57a134f0fcd45aeee7e
MihajloZivkovic1 <mikibunja@gmail.com>
2024-04-11 07:28:04 -0700

    sale car

--- proba.c
+++ proba.c
@@ -1,8 +1,10 @@
 include<stdio.h>
 int main() {
+
   printf("Hello world!");
   printf("Hello world!");
   printf("Hello world!");
- printf("Hello world!");
+  printf("Hello world!");
+  printf("CAOOOOO SALEEEEE")
 
 }
